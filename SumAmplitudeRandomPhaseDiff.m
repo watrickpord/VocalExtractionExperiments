a6ce28ad6bb474d69bcfc049bdf33878039b68f7
Patrick <58635201+watrickpord@users.noreply.github.com>
2020-11-14 20:45:42 +0000

    Added normal and logistic fit to amplitude/sigma
    Normal fit fairly well, logistic not well at all

--- SumAmplitudeRandomPhaseDiff.m
+++ SumAmplitudeRandomPhaseDiff.m
@@ -4,29 +4,51 @@
 % indefinite integral of this function wrt x gives amplitude for given sigma
 Integrand = @(sigma, x) 1./(sigma.*sqrt(pi)).*exp(-0.5*(x./sigma).^2).*sqrt(1+cos(x));
 
-% loop over sigma values and calculate indefinite integral for each
+% we'll loop over sigma values and calculate indefinite integral for each
 sigmas = 0.1:0.1:10;
-
 Integrals = zeros(size(sigmas));
 
-% range of integration
-phi = -100:0.01:100;
+phi = -100:0.01:100;    % range of integration
 
 % for plotting integrands
-figure
-hold on
+%figure
+%hold on
 
 for index = 0:range(size(sigmas))
     currentIntegrand = Integrand(sigmas(index+1), phi);
-    plot(phi, currentIntegrand)
+    %plot(phi, currentIntegrand)
     
     currentIntegral = -trapz(currentIntegrand, phi);
     Integrals(index+1) = currentIntegral;
 end
 
+% plot ampltidue vs. sigma
 figure
 plot(sigmas, Integrals)
 title('Expected Amplitude for Sum of 2 Sines with Gaussian Phase Difference Probability')
 xlabel('Std. Dev. of Phase Difference Gaussian')
 ylabel('Amplitude')
 grid on
+
+% fit resulting curve to normal distrobution
+initGuess = [1,1,1];    % initial guess for amplitude, std dev, and +c
+[fit, residual] = fminsearch(@(p) norm( p(1).*exp(-0.5.*((sigmas(:))./p(2)).^2) + p(3) -Integrals(:)), initGuess)
+fit
+residual
+
+% plot normal fit
+hold on
+plot(sigmas, fit(1).*exp(-0.5.*((sigmas(:))./fit(2)).^2) + fit(3));
+
+
+% logistic fit
+initGuess = [0.7,2,2,1.2722];    % initial guess for amplitude, exponent, x0, and +c
+[fit, residual,exitflag] = fminsearch(@(p) norm( p(1)./( 1 + exp(p(2)*(sigmas-p(3))) ) + p(4) -Integrals(:)), initGuess)
+fit
+residual
+exitflag
+
+% plot logistic fit
+hold on
+plot(sigmas, fit(1)./(1 + exp(fit(2)*(sigmas-fit(3)))) + fit(4));
+%     ------> doesn't work for some reason
